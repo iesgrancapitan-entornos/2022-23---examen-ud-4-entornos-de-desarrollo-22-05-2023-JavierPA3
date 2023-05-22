3187ec7feb9db0bd85e0784a294d476f397f69fa
JavierPA3 <a21poarja@iesgrancapitan.org>
2023-05-22 14:09:53 +0200

    Documentacion 1 - Javier Postigo Arévalo

--- Perro.py
+++ Perro.py
@@ -1,15 +1,18 @@
 """
 Clase Perro.
-
-Autor: Jaime Rabasco Ronda.
+:Date: 2023-05-22
+:autor: Jaime Rabasco Ronda.
 """
 class Perro:
 
     def __init__(self) :
+        """ Creamos el constructor con el atributo/campo self.ladrar que inicializara con guau"""
         self.ladrar = 'Guau'
 
     def ladrar(self):
+        """Esta función hace que nuestro 'perro' ladre, tiene que salir por pantalla guau"""
         print(self.ladrar);
 
+"""Creamos el objeto perro y hacemos que ladre."""
 p = Perro();
 p.ladrar();
